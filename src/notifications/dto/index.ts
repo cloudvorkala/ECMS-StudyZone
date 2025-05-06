403527232083d7445eafbbcbf70e7a68f512ccdb
cloudvorkala <cloudvorkala@gmail.com>
2025-05-06 17:11:04 +1200

    new development startpoint

--- src/notifications/dto/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all DTOs from index file
-// src/notifications/dto/index.ts
-export * from './create-notification.dto';
-export * from './notification-response.dto';
-export * from './notification-query.dto';
