bbdc00d3a208950c1ea25bf54a18bb234ad61fa4
nfd8672 <165477531+nfd8672@users.noreply.github.com>
2025-05-07 11:18:48 +1200

    ready for merge

--- src/common/decorators/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-/**
- * 路由级别的角色要求装饰器
- * 使用方式: @Roles('admin', 'mentor')
- */
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
--- src/help-requests/dto/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all DTOs from index file
-// src/help-requests/dto/index.ts
-export * from './create-help-request.dto';
-export * from './update-help-request.dto';
-export * from './create-help-response.dto';
-export * from './assign-help-request.dto';
-export * from './help-request-query.dto';
